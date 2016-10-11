c7aac56f34bc9b35d4e1b5798cddb615cbebb5aa
Yujing Wu <yujingwu@gmail.com>
2016-10-11 15:13:20 -0700

    DX-648 Enhance the code and comments

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var mqtt = require('mqtt')
+var mqtt = require('mqtt’);
 var CONFIG = require('./config.json');
 
+// The port number is specified at
+// https://developer.artik.cloud/documentation/connect-the-data/mqtt.html#key-concepts
 var credentials = {
+  port: 8883,
   username: CONFIG.DEVICE_ID,
   password: CONFIG.DEVICE_TOKEN
 }
 
-var client  = mqtt.connect('mqtts://api.artik.cloud', credentials);
+// Per https://www.npmjs.com/package/mqtt#client, the URL can be on the following protocols: 
+// 'mqtt', 'mqtts', 'tcp', 'tls', 'ws', 'wss'
+//
+// For ARTIK Cloud, mqtt client must be SSL-capable. 
+// ’tls’ and ‘mqtts’ protocols work
+var client  = mqtt.connect('tls://api.artik.cloud', credentials);
+//var client  = mqtt.connect('mqtts://api.artik.cloud', credentials); //This is also working
 
 // ARTIK Cloud only allows the following 2 paths on MQTT
 var PUBLISH_MESSAGE_PATH = "/v1.1/messages/" + CONFIG.DEVICE_ID;
@@ -14,16 +23,16 @@ var SUBSCRIBE_ACTION_PATH = "/v1.1/actions/" + CONFIG.DEVICE_ID;
 
 
 client.on('connect', function () {
-
  
   var sampleData = getSampleData();
   console.log("publishing data:", sampleData)
   console.log("publish path:", PUBLISH_MESSAGE_PATH);
 
-  client.subscribe(SUBSCRIBE_ACTION_PATH);
   client.publish(PUBLISH_MESSAGE_PATH, getSampleData());
-
   console.log("Use browser to see your data in realtime https://artik.cloud/my/data")
+
+  // Example for subscribing to receive Action
+  // client.subscribe(SUBSCRIBE_ACTION_PATH);
 
 })
 
@@ -32,9 +41,7 @@ client.on('message', function (topic, message) {
   client.end()
 })
 
-//
 function getSampleData() {
-
 	//fields key/value for you ARTIK Cloud device
 	return JSON.stringify({
 	  "temp": 214
